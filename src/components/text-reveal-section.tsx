6c74750633e8c56c4a378c9017d2d23849de9aa2
Nainish Rai <nainishraisjp@gmail.com>
2025-07-25 23:57:55 +0530

    fix: ts errors

--- src/components/text-reveal-section.tsx
+++ src/components/text-reveal-section.tsx
@@ -6,7 +6,7 @@ const TextRevealSection = () => {
   return (
     <section className="dark bg-background my-0 ">
       <div className=" flex flex-col items-center justify-center">
-        <TextReveal className="font-medium" maxWidth="86rem">
+        <TextReveal className="font-medium">
           OpenWork is the open source framework designed to empower developers
           and organizations to build, deploy, and manage advanced AI agents at
           scale. Seamlessly integrate AI capabilities into your applications.
